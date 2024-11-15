0da4ca4e59de2bc5cf61efecee4db8fcf32683e8
Zlata <zolata9090@gmail.com>
2024-11-15 19:04:35 +0200

    added js

--- script.js
+++ script.js
@@ -1,40 +1,72 @@
-let showAddBtn = document.querySelector("#show-add-btn")
-let showListBtn = document.querySelector("#show-list-btn")
-let showFavoriteBtn = document.querySelector("#show-favorite-btn")
 
-let addScreen = document.querySelector(".todo__app")
-let listScreen = document.querySelector(".todo__list")
-let favoriteScreen = document.querySelector(".todo__favorite")
+let firstBtn = document.querySelector("#show-add-btn")   // перша кнопка
+let secondBtn = document.querySelector("#show-list-btn")    // друга кнопка
+let thirdBtn = document.querySelector("#show-favorite-btn")     // третя кнопка
+let firstScreen = document.querySelector(".todo__app")    // перший екран
+let secondcreen = document.querySelector(".todo__list")   // другий екран
+let thirdScreen = document.querySelector(".todo__favorite")   // третій екран
 
-addScreen.classList.add("show")
-showAddBtn.classList.add("show")
+let saveBtn = document.querySelector(".save")
+let firstScreenText = document.querySelector("textarea")
+let secondScreenList = document.querySelector(".todo__list-wrapper")
+let form = document.querySelector("form")
 
-showAddBtn.addEventListener("click", ()=>{
-    addScreen.classList.add("show")
-    listScreen.classList.remove("show")
-    favoriteScreen.classList.remove("show")
+firstScreen.classList.add("show")   // за замовчуванням показати перший екран (першу половинку)
+firstBtn.classList.add("paint")  // за замовчуванням покрасити першу кнопку у інший колір
 
-    showAddBtn.classList.add("show")
-    showListBtn.classList.remove("show")
-    showFavoriteBtn.classList.remove("show")
+firstBtn.addEventListener("click", () => {   // коли ми клікаємо на першу кнопку то виконуємо код нижче
+  firstScreen.classList.add("show")   // показати перший екран
+  secondcreen.classList.remove("show")    // прибрати другий екран
+  thirdScreen.classList.remove("show")    // прибрати третій екран
+
+  firstBtn.classList.add("paint")     //   покрасити першу кнопку у червоний кодлір
+  secondBtn.classList.remove("paint")   // прибрати червоний колір кнопки
+  thirdBtn.classList.remove("paint")   // прибрати червоний колір кнопки
 })
 
-showListBtn.addEventListener("click",()=>{
-    addScreen.classList.remove("show")
-    listScreen.classList.add("show")
-    favoriteScreen.classList.remove("show")
+secondBtn.addEventListener("click", () => {  // коли ми клікаємо на другу кнопку то виконуємо код нижче
+  firstScreen.classList.remove("show")   // прибрати перший екран
+  secondcreen.classList.add("show")   // показати другий екран
+  thirdScreen.classList.remove("show")  // прибрати третій екран
 
-    showAddBtn.classList.remove("show")
-    showListBtn.classList.add("show")
-    showFavoriteBtn.classList.remove("show")
+  firstBtn.classList.remove("paint")   // прибрати червоний колір кнопки 1
+  secondBtn.classList.add("paint")   // додаємо червоний колір кнопки 2
+  thirdBtn.classList.remove("paint")    //  прибрати червоний колір кнопки 3
 })
 
-showFavoriteBtn.addEventListener("click",() =>{
-    addScreen.classList.remove("show")
-    listScreen.classList.remove("show")
-    favoriteScreen.classList.add("show")
+thirdBtn.addEventListener("click", () => {
+  firstScreen.classList.remove("show")
+  secondcreen.classList.remove("show")
+  thirdScreen.classList.add("show")
 
-    showAddBtn.classList.remove("show")
-    showListBtn.classList.remove("show")
-    showFavoriteBtn.classList.add("show")
+  firstBtn.classList.remove("paint")
+  secondBtn.classList.remove("paint")
+  thirdBtn.classList.add("paint")
+})
+
+form.addEventListener("submit", (e) => {e.preventDefault()})//відміняе перезагрузку при кліку на кнопку save
+saveBtn.addEventListener("click", () =>{//добавляе подію клик на кнопку сейв
+
+  
+  secondScreenList.innerHTML +=`
+   <div class="todo__list-item">
+                        <p class="todo__list-content">${firstScreenText.value}</p>
+                        <div class="todo__list-controll">
+                            <div class="todo__list_control_btns">
+                                <input class="status" type="checkbox" name="" id="">
+                                <button> <img src="./assets/icon/star.svg" alt="star"></button>
+                                <button class="delete"> <img src="./assets/icon/rubbish bin.svg" alt="star"></button>
+                            </div>
+                            <p>18/10/2024</p>
+                        </div>
+                    </div>
+
+  `
+  firstScreenText.value = ""
+})
+
+secondScreenList.addEventListener("click", (e) =>{
+if(e.target.closest(".delete")){
+  alert("Привіт, Злата")
+}
 })
